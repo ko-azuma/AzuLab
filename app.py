b3723af7a536c574b701aa402ae63abd352d7258
k_azuma <k.azuma@avanzare-tech.jp>
2026-08-03 01:45:06 +0900

    最終

--- app.py
+++ app.py
@@ -469,8 +469,6 @@ def create_initial_user():
 
     db.session.commit()
 
-    db = SQLAlchemy(app)
-
 with app.app_context():
     db.create_all()
     create_initial_user()
@@ -1246,15 +1244,5 @@ def unlock_account(token):
 # 実行
 # =========================
 if __name__ == "__main__":
-
-    with app.app_context():
-
-        db.create_all()
-
-        if os.environ.get(
-            "WERKZEUG_RUN_MAIN"
-        ) == "true":
-
-            create_initial_user()
 
     app.run(debug=True)
